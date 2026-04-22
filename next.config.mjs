dfc6ed15765c74ed0748f84366c4e261e2d3e74c
Thiendmh <dinhducthien@gmail.com>
2026-04-22 10:31:35 +0700

    Configure deployment to GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // If you are deploying to https://<username>.github.io/<repository-name>/
+  basePath: '/KnowDellCardSorts',
 };
 
 export default nextConfig;
+
